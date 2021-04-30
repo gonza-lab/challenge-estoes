63c1a0630d7040a4b49bdd18af7537ca6b69a70f
Gonzalo Flores <gonzaflores358@gmail.com>
2021-04-30 19:06:41 -0300

    front end terminado

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,28 +1,29 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import {
   BrowserRouter as Router,
   Redirect,
   Route,
   Switch,
 } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import project from './redux/project/actions';
 import { Header } from './components/header/Header';
 import { ScreensListRoot } from './screens/list/Root';
-import { ScreensAddEditRoot } from './screens/add-edit/Root'
+import { ScreensAddEditRoot } from './screens/add-edit/Root';
 import './styles/styles.scss';
 
 function App() {
   const dispatch = useDispatch();
-
-  useEffect(() => {}, []);
-
+  useEffect(() => {
+    dispatch(project.startReadAll());
+  });
   return (
     <Router>
       <Header />
       <Switch>
         <Route exact path="/" component={ScreensListRoot} />
-	<Route exact path="/add" component={ScreensAddEditRoot} />
-	<Route exact path="/edit/:id" component={ScreensAddEditRoot} />
+        <Route exact path="/add" component={ScreensAddEditRoot} />
+        <Route exact path="/edit/:id" component={ScreensAddEditRoot} />
         <Redirect to="/" />
       </Switch>
     </Router>
